e32eb039078fcfacb0c6dcd3b4605347d336e7fa
Daniel Young <daniel.young@bluefrontier.co.uk>
2019-08-14 17:31:09 +0100

    feat: adding message consumption

---
+++ app.js
@@ -0,0 +1,16 @@
+const amqp = require("amqplib");
+const { amqpAddress, noAuthRawMessageQ } = require("./config");
+const { logger } = require("./logger");
+
+amqp.connect()
+    .then(function(conn) {
+        return conn.createChannel().then(ch => {
+            var handleMessage = m => {
+                logger.info(m.content.toString("utf-8"));
+                ch.ack(m);
+            };
+
+            return ch.consume(noAuthRawMessageQ, handleMessage);
+        });
+    })
+    .catch(console.warn);
