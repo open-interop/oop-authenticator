daadce64511bfe1e6630ad9a99cf0c0c8042aed4
danielyoungbf <43783921+danielyoungbf@users.noreply.github.com>
2019-08-16 16:10:28 +0100

    feat: authenticating messages (#3)

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
 const amqp = require("amqplib");
-const { amqpAddress, noAuthRawMessageQ } = require("./config");
+const config = require("./config");
 const { logger } = require("./logger");
+const Authenticator = require("./lib/Authenticator");
+const data = require("./fake-device-data");
 
-amqp.connect(amqpAddress)
-    .then(function(conn) {
+Authenticator.importDevices(data);
+
+amqp.connect(config.amqpAddress)
+    .then(conn => {
         return conn.createChannel().then(ch => {
-            var handleMessage = m => {
-                logger.info(m.content.toString("utf-8"));
-                ch.ack(m);
+            var handleMessage = message => {
+                try {
+                    var content = message.content.toString("utf-8");
+                    var messageData = JSON.parse(content);
+
+                    logger.info(`Message originated from ${messageData.ip}.`);
+
+                    var device = Authenticator.authenticateMessage(messageData);
+
+                    if (device !== false) {
+                        logger.info(
+                            `Message originated from ${messageData.ip} authenticated.`
+                        );
+
+                        var toSend = {
+                            device: device,
+                            message: messageData
+                        };
+
+                        var json = JSON.stringify(toSend);
+                        if (
+                            ch.publish(
+                                config.exchangeName,
+                                config.hasAuthRawMessageQ,
+                                Buffer.from(json)
+                            )
+                        ) {
+                            ch.ack(message);
+                        } else {
+                            throw new Error(
+                                "Unable to publish to authenticated queue."
+                            );
+                        }
+                    } else {
+                        ch.ack(message);
+
+                        logger.info(
+                            `Message originated from ${messageData.ip} not authenticated.`
+                        );
+                    }
+                } catch (e) {
+                    ch.nack(message);
+
+                    logger.error(String(e));
+                }
             };
 
-            return ch.consume(noAuthRawMessageQ, handleMessage);
+            return ch.consume(config.noAuthRawMessageQ, handleMessage);
         });
     })
-    .catch(console.warn);
+    .catch(logger.error);
+
