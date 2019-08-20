d06abd8ae2138be4c346ec702149cbaf2dc3e59b
danielyoungbf <43783921+danielyoungbf@users.noreply.github.com>
2019-08-20 17:26:51 +0100

    feat: adding device updating from message queue (#6)

--- config.js
+++ config.js
@@ -4,5 +4,9 @@ dotenv.config();
 
 module.exports = {
     amqpAddress: process.env.AMQP_ADDRESS,
-    noAuthRawMessageQ: process.env.NO_AUTH_RAW_MESSAGE_Q
+    noAuthRawMessageQ: process.env.NO_AUTH_RAW_MESSAGE_Q,
+    exchangeName: process.env.EXCHANGE_NAME,
+    hasAuthRawMessageQ: process.env.HAS_AUTH_RAW_MESSAGE_Q,
+    oopCoreDeviceEndpoint: process.env.OOP_CORE_DEVICE_ENDPOINT,
+    oopCoreDeviceUpdateExchange: process.env.OOP_CORE_DEVICE_UPDATE_EXCHANGE
 };
