90f39e7cff47e8bba298d7f50dba6fe66f012d83
danielyoungbf <43783921+danielyoungbf@users.noreply.github.com>
2019-08-22 14:38:28 +0100

    fix: updating message object format and logs (#8)

--- config.js
+++ config.js
@@ -4,9 +4,11 @@ dotenv.config();
 
 module.exports = {
     amqpAddress: process.env.AMQP_ADDRESS,
-    noAuthRawMessageQ: process.env.NO_AUTH_RAW_MESSAGE_Q,
     exchangeName: process.env.EXCHANGE_NAME,
+    oopCoreToken: process.env.OOP_CORE_TOKEN,
+    noAuthRawMessageQ: process.env.NO_AUTH_RAW_MESSAGE_Q,
     hasAuthRawMessageQ: process.env.HAS_AUTH_RAW_MESSAGE_Q,
     oopCoreDeviceEndpoint: process.env.OOP_CORE_DEVICE_ENDPOINT,
-    oopCoreDeviceUpdateExchange: process.env.OOP_CORE_DEVICE_UPDATE_EXCHANGE
+    oopCoreDeviceUpdateExchange: process.env.OOP_CORE_DEVICE_UPDATE_EXCHANGE,
+    oopCoreApiUrl: process.env.OOP_CORE_API_URL
 };
