aacd8cef6b06e69952e1fd6727933c261d860f10
danielyoungbf <43783921+danielyoungbf@users.noreply.github.com>
2019-08-23 17:18:35 +0100

    fix: renaming env vars (#10)

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    amqpAddress: process.env.AMQP_ADDRESS,
-    exchangeName: process.env.EXCHANGE_NAME,
+    amqpAddress: process.env.OOP_AMQP_ADDRESS,
+    exchangeName: process.env.OOP_EXCHANGE_NAME,
     oopCoreToken: process.env.OOP_CORE_TOKEN,
-    noAuthRawMessageQ: process.env.NO_AUTH_RAW_MESSAGE_Q,
-    hasAuthRawMessageQ: process.env.HAS_AUTH_RAW_MESSAGE_Q,
+    noAuthRawMessageQ: process.env.OOP_NOAUTH_RAW_MESSAGE_Q,
+    hasAuthRawMessageQ: process.env.OOP_HASAUTH_RAW_MESSAGE_Q,
     oopCoreDeviceEndpoint: process.env.OOP_CORE_DEVICE_ENDPOINT,
     oopCoreDeviceUpdateExchange: process.env.OOP_CORE_DEVICE_UPDATE_EXCHANGE,
     oopCoreApiUrl: process.env.OOP_CORE_API_URL
